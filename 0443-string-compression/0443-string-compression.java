a44d1f87fc97c91a2f9ddcbc6416097b28c42234
KartikSuchak <157241558+KartikSuchak@users.noreply.github.com>
2026-02-02 23:51:38 +0530

    Time: 1 ms (99.65%), Space: 45.7 MB (17.01%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int compress(char[] chars) {
+        if (chars.length <= 1) {
+            return chars.length;
+        }
+
+        int currIndex = 0;
+        int count = 1;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                count++;
+            } else {
+                if (count > 1) {
+                    for (char c : String.valueOf(count).toCharArray()) {
+                        chars[++currIndex] = c;
+                    }
+                }
+                chars[++currIndex] = chars[i];
+                count = 1;
+            }
+        }
+        if (count > 1) {
+            for (char c : String.valueOf(count).toCharArray()) {
+                chars[++currIndex] = c;
+            }
+        }
+        return currIndex + 1;
+    }
+}
