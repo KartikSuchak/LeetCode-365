25134b2f2ef00719082bdb8895b632d1f3b7679b
KartikSuchak <157241558+KartikSuchak@users.noreply.github.com>
2026-02-14 23:38:30 +0530

    Time: 2 ms (98.59%), Space: 48.4 MB (39.22%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.java
+++ 0799-champagne-tower/0799-champagne-tower.java
@@ -1,6 +1,6 @@
 class Solution {
     public double champagneTower(int poured, int query_row, int query_glass) {
-        double[][] tower = new double[102][102];
+        double[][] tower = new double[query_row + 2][query_row + 2];
         tower[0][0] = (double) poured;
 
         for (int i = 0; i <= query_row; i++) {
