eb9b0a7c7e76676b1621d0fc41d0ca3bfb8b74c0
KartikSuchak <157241558+KartikSuchak@users.noreply.github.com>
2026-03-09 01:19:53 +0530

    Time: 0 ms (100%), Space: 43.1 MB (87.73%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.java
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -1,21 +1,10 @@
 class Solution {
     public String findDifferentBinaryString(String[] nums) {
         int m = nums.length;
-        int space = (int)Math.pow(2,m);
-        int[] strings = new int[space];
-
-        for(String num : nums){
-            int val = Integer.parseInt(num, 2);
-            strings[val]++;
+        char[] ans = new char[m];
+        for (int i = 0; i < m; i++){
+            ans[i] = nums[i].charAt(i) == '0' ? '1' : '0';
         }
-
-        for(int i = 0; i < space; i++){
-            if(strings[i] == 0){
-                String ans = Integer.toBinaryString(i);
-                return "0".repeat(m - ans.length()) + ans;
-            }
-        }
-
-        return "0".repeat(m);
+        return new String(ans);
     }
 }
