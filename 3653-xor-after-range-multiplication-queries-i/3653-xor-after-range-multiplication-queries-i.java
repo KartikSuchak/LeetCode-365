c3ad0a5dafb81c660347a549acb19dca5f9e849f
KartikSuchak <157241558+KartikSuchak@users.noreply.github.com>
2026-04-08 23:47:48 +0530

    Time: 60 ms (57.33%), Space: 48.2 MB (17.33%) - LeetHub

--- 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.java
@@ -1,16 +1,24 @@
 class Solution {
     static final int M = 1000000007;
     public int xorAfterQueries(int[] nums, int[][] queries) {
+        int n = nums.length;
+        long[] mul = new long[n];
+        for (int i = 0; i < n; i++) {
+            mul[i] = 1;
+        }
         for (int i = 0; i < queries.length; i++) {
-            long idx = queries[i][0];
-            while (idx <= queries[i][1]) {
-                nums[(int)idx] = (int)((1L * nums[(int)idx] * queries[i][3]) % M);
-                idx += queries[i][2];
+            int l = queries[i][0];
+            int r = queries[i][1];
+            int k = queries[i][2];
+            int v = queries[i][3];
+            for (int idx = l; idx <= r; idx += k) {
+                mul[idx] = (mul[idx] * v) % M;
             }
         }
-        int ans = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            ans = ans ^ nums[i];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int val = (int)((nums[i] * mul[i]) % M);
+            ans = ans ^ val;
         }
         return ans;
     }
